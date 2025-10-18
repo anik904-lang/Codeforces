96b24e32c41c48af01ee69eeec52dadaa088d4d7
anik904-lang <hossainanik904@gmail.com>
2025-10-18 22:19:52 +0600

    Add files via upload

---
+++ cdf-41A.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    string s,t;
+    cin>>s;
+    cin>>t;
+    reverse(s.begin(),s.end());
+    if(s==t)
+        cout<<"YES";
+    else
+        cout<<"NO";
+    return 0;
+}
+
