41cae73724acfb5a3dbfc9ebc32c26de55af6720
anik904-lang <hossainanik904@gmail.com>
2025-10-31 10:35:45 +0600

    Add files via upload

---
+++ cdf-133A.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string p;
+    cin>>p;
+    
+for(char c:p){
+    if(c=='H' || c=='Q' || c=='9'){
+        cout<<"YES";
+       return 0;
+    }
+}
+cout<<"NO";
+ return 0;
+}
