df855d67bff67e36f40938408b6fb68196295db9
Anik Hossain <hossainanik904@gmail.com>
2025-12-16 22:17:36 +0600

    Add files via upload

---
+++ cdf-996A.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int n;
+    cin>>n;
+    int a[]={100,20,10,5,1};
+     int count=0;
+    
+     for (int i = 0; i < 5; i++) {
+        count += n / a[i];
+        n %= a[i];
+        
+    if (n == 0) {
+            break;
+        }
+    }
+     cout<<count;
+     return 0;
+    
+}
